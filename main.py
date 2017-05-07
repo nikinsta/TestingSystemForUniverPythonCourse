443c8c68623b82f036fb8775204b7a47f6c7c6b8
Nik <nik.machula@yandex.ru>
2017-05-07 17:26:18 +0400

    Added functionality to variants_frame widgets. Improved style.

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ import resourses.constants
 from tkinter import *
 
 
-def disable_item(user_answers, item_id):
+def disable_item(user_answers, item_id, variants_frame, parsed):
     user_answers[item_id]['disabled'] = True
+    vscopy = variants_frame.children.copy()
+    for key, widget in vscopy.items():
+        widget.config(state=DISABLED)
+        # saving widgets state
+        item_type = parsed[str(item_id)]['type']
+        if item_type == 'entry':
+            user_answers[item_id]['answers'].clear()
+            user_answers[item_id]['answers'].add(widget.get())
+        elif item_type == 'checkbutton':
+            continue
+        elif item_type == 'radiobutton':
+            continue
+        else:
+            print('ERROR in disable_item')
 
 
 def destroy_widgets_in_variants_frame(entry, checkbox, radiobox):
@@ -29,11 +43,33 @@ def check_all_comfirmed(user_answers, win):
     win.destroy()
 
 
-def go_to_question(item_id, parsed, nav_buttons, msg, variants_frame, btn_prev, btn_comfirm, btn_next, user_answers,
+def toggle_checkbutton(checkbutton_var, item_id, variant_id, user_answers):
+    value = checkbutton_var.get()
+    print('chkbut value :', value)
+    if value == 1:
+        user_answers[item_id]['answers'].add(variant_id)
+    elif value == 0:
+        if variant_id in user_answers[item_id]['answers']:
+            user_answers[item_id]['answers'].remove(variant_id)
+
+
+def select_radiobutton(radiobutton_var, item_id, variant_id, user_answers):
+    value = radiobutton_var.get()
+    print('radiobtn value :', value)
+    user_answers[item_id]['answers'].clear()
+    user_answers[item_id]['answers'].add(variant_id)
+
+
+def go_to_question(item_id, prev_item_id, parsed, nav_buttons, msg, variants_frame, btn_prev, btn_comfirm, btn_next,
+                   user_answers,
                    win):
     print('go to question ', item_id)
     # print('user_answers', user_answers)
     # print(nav_buttons)
+
+    # saving widgets state
+    # if parsed[str(item_id)]['type'] == 'entry':
+    #   user_answers[item_id]['answers'] = {}
 
     # print('before', variants_frame.children)
     vscopy = variants_frame.children.copy()
@@ -45,7 +81,7 @@ def go_to_question(item_id, parsed, nav_buttons, msg, variants_frame, btn_prev, 
 
     if item_id > 0:
         btn_prev.config(state=NORMAL)
-        btn_prev.config(command=(lambda: go_to_question(item_id - 1, parsed, nav_buttons, msg, variants_frame,
+        btn_prev.config(command=(lambda: go_to_question(item_id - 1, item_id, parsed, nav_buttons, msg, variants_frame,
                                                         btn_prev, btn_comfirm, btn_next, user_answers, win)))
     else:
         btn_prev.config(state=DISABLED)
@@ -60,14 +96,16 @@ def go_to_question(item_id, parsed, nav_buttons, msg, variants_frame, btn_prev, 
         btn_comfirm.config(state=NORMAL)
         btn_comfirm.config(
             command=(lambda: nav_buttons[item_id].config(bg='green')
-                             or disable_item(user_answers, item_id) or btn_comfirm.config(state=DISABLED)
+                             or disable_item(user_answers, item_id, variants_frame, parsed)
+                             or btn_comfirm.config(state=DISABLED)
                              or check_all_comfirmed(user_answers, win)))
 
     if item_id + 1 < len(parsed):
         btn_next.config(state=NORMAL)
         btn_next.config(
             command=(
-                lambda: go_to_question(item_id + 1, parsed, nav_buttons, msg, variants_frame, btn_prev, btn_comfirm,
+                lambda: go_to_question(item_id + 1, item_id, parsed, nav_buttons, msg, variants_frame, btn_prev,
+                                       btn_comfirm,
                                        btn_next, user_answers, win)))
     else:
         btn_next.config(state=DISABLED)
@@ -86,29 +124,92 @@ def go_to_question(item_id, parsed, nav_buttons, msg, variants_frame, btn_prev, 
         entry = Entry(variants_frame)
         entry.config(font=resourses.constants.ENTRY_FONT)
         entry.config(width=30)
+        # http: // infohost.nmt.edu / tcc / help / pubs / tkinter / web / cursors.html
+        # cursor ::= 'gumby', 'watch', 'pencil', 'cross', 'hand2'
+        entry.config(cursor='xterm')
+        entry.config(show='', insertofftime=500, insertontime=500)
         entry.pack(expand=YES, padx=10, pady=10)
-        entry.config(text='asd')
+        entry.focus_set()
 
-        # Button(variants_frame, text='OK', command=win.destroy).pack()
+        # entry_text = StringVar()
+        # entry.config(variable=entry_text)
+
+        # restore state of the entry
+        if len(user_answers[item_id]['answers']) != 0:
+            entry.insert(0, list(user_answers[item_id]['answers'])[0])
+        else:
+            entry.insert(0, '')
+
+        if user_answers[item_id]['disabled']:
+            entry.config(state=DISABLED)
+        else:
+            entry.config(state=NORMAL)
+
+            # user_answers[item_id]['answers']['0'] = entry.get()
+            # print(user_answers[item_id]['answers']['0'])
+
+            # root.geometry(newGeometry='300x300+200+200')
+
+            # Button(variants_frame, text='OK', command=win.destroy).pack()
 
     elif item_type == 'checkbutton':
         # return
+        disabled = user_answers[item_id]['disabled']
+
         checkbox = []
         for variant_id in map(str, range(len(variants))):
             variant = variants[variant_id]
             checkbutton = Checkbutton(variants_frame)
-            checkbutton.config(text=variant)
-            checkbutton.pack()
+            checkbutton.config(cursor='hand2')
+            checkbutton.config(text=variant, font=resourses.constants.CHECKBUTTON_TEXT_FONT, wraplength=400)
+            checkbutton_var = IntVar()
+            checkbutton.config(variable=checkbutton_var)
+            checkbutton.config(command=(
+                lambda item_id=item_id, variant_id=variant_id, checkbutton_var=checkbutton_var: toggle_checkbutton(
+                    checkbutton_var, item_id, variant_id,
+                    user_answers)))
+            checkbutton.pack(side=TOP, anchor=NW, padx=150)
             checkbox.append(checkbutton)
+
+            # restore state of the checkbutton
+            if variant_id in user_answers[item_id]['answers']:
+                checkbutton.toggle()
+            # print(user_answers[item_id]['answers'])
+            if disabled:
+                checkbutton.config(state=DISABLED)
+            else:
+                checkbutton.config(state=NORMAL)
+
+
     elif item_type == 'radiobutton':
         # return
+        disabled = user_answers[item_id]['disabled']
+
         radiobox = []
+        radiobutton_var = IntVar()
+        radiobutton_var.set(-1)
+
         for variant_id in map(str, range(len(variants))):
             variant = variants[variant_id]
             radiobutton = Radiobutton(variants_frame)
-            radiobutton.config(text=variant)
-            radiobutton.pack()
+            radiobutton.config(text=variant, font=resourses.constants.CHECKBUTTON_TEXT_FONT, wraplength=400)
+            radiobutton.config(cursor='hand2')
+            radiobutton.config(variable=radiobutton_var, value=variant_id)
+            radiobutton.config(command=(lambda item_id=item_id, variant_id=variant_id, radiobutton_var=radiobutton_var:
+                                        select_radiobutton(radiobutton_var, item_id, variant_id, user_answers)))
+            radiobutton.pack(side=TOP, anchor=NW, padx=150)
             radiobox.append(radiobutton)
+
+            # restore state of the radiobutton
+            if variant_id in user_answers[item_id]['answers']:
+                radiobutton.select()
+            else:
+                radiobutton.deselect()
+
+            if disabled:
+                radiobutton.config(state=DISABLED)
+            else:
+                radiobutton.config(state=NORMAL)
     else:
         print('UNEXPECTED ITEM TYPE (should be entry or checkbutton or radiobutton)')
 
@@ -127,13 +228,13 @@ def get_results(user_answers, parsed):
     # print(parsed['0']['answers'])
 
     for i in range(len(user_answers)):
-        if user_answers[i]['answers'] == parsed[str(i)]['answers']:
+        if user_answers[i]['answers'] == set(parsed[str(i)]['answers'].values()):
             count += 1
     return count, len(parsed)
 
 
 def testing(root, parsed):
-    user_answers = {i: {'disabled': False, 'answers': {}} for i in range(len(parsed))}
+    user_answers = {i: {'disabled': False, 'answers': set()} for i in range(len(parsed))}
     # user_answers[0]['disabled'] = True
     # user_answers[1]['answers'] = {1, 2}
     # user_answers[2] = {'disabled': False, 'answers': {3, 4, 5}}
@@ -157,8 +258,9 @@ def testing(root, parsed):
         btn.config(text=str(item_id + 1), font=resourses.constants.BUTTON_FONT)
         btn.config(height=1, width=3)
         btn.config(overrelief=RAISED, activebackground='#DDD')
-        btn.config(command=(lambda item_id=item_id: go_to_question(item_id, parsed, nav_buttons, msg, variants_frame,
-                                                                   btn_prev, btn_comfirm, btn_next, user_answers, win)))
+        btn.config(
+            command=(lambda item_id=item_id: go_to_question(item_id, -1, parsed, nav_buttons, msg, variants_frame,
+                                                            btn_prev, btn_comfirm, btn_next, user_answers, win)))
         btn.pack(side=LEFT, padx=10, pady=10)
         nav_buttons.append(btn)
     nav_frame.pack(expand=NO, fill=BOTH)
@@ -195,7 +297,7 @@ def testing(root, parsed):
     btn_next.config(overrelief=RAISED, activebackground='#DDD')
     btn_next.pack(side=RIGHT, expand=YES, fill=BOTH, padx=20, pady=20)
 
-    go_to_question(0, parsed, nav_buttons, msg, variants_frame, btn_prev, btn_comfirm, btn_next, user_answers, win)
+    go_to_question(0, -1, parsed, nav_buttons, msg, variants_frame, btn_prev, btn_comfirm, btn_next, user_answers, win)
 
     # win.protocol('WM_DELETE_WINDOW', win.quit)
     win.focus_set()
@@ -210,14 +312,26 @@ def testing(root, parsed):
     btn_start.config(text='Начать заново')
 
     user_result, total = get_results(user_answers, parsed)
-    rel = user_result
+    # user_result = 9
+    welcome_msg_text = str(user_result) + ' / ' + str(total)
+    normalized_user_result = user_result / total
+    if normalized_user_result <= 0.4:
+        welcome_msg_fg_color = 'red'
+        welcome_msg_text += '\nПлохой результат'
+    elif normalized_user_result <= 0.75:
+        welcome_msg_fg_color = 'blue'
+        welcome_msg_text += '\nХороший результат'
+    else:
+        welcome_msg_fg_color = 'green'
+        welcome_msg_text += '\nОтличный результат'
 
-    welcome_msg.config(text=results)
+    welcome_msg.config(text=welcome_msg_text, fg=welcome_msg_fg_color, font=resourses.constants.TEST_RESULT_FONT)
 
 
 if __name__ == '__main__':
     path = 'resourses/questions.xml'
     parsed = xml_parser.parse(path)
+    # parsed.popitem()
 
     root = Tk()
     root.minsize(width=600, height=500)
@@ -240,7 +354,7 @@ if __name__ == '__main__':
 
     footer_frame = Frame(main_frame)
     footer_frame.config(bg="#FFF")
-    footer_frame.pack(expand=YES, fill=BOTH)
+    footer_frame.pack(expand=NO, fill=BOTH)
 
     btn_start = Button(footer_frame, text='Начать тестирование', command=(lambda: testing(root, parsed)))
     btn_start.config(font=resourses.constants.BUTTON_FONT, bg="#BBB")
--- resourses/constants.py
+++ resourses/constants.py
@@ -1,6 +1,8 @@
 MESSAGE_FONT = ('consolas', 20, 'roman')
 BUTTON_FONT = ('times', 20, '')
 ENTRY_FONT = ('times', 20, '')
+CHECKBUTTON_TEXT_FONT = ('consolas', 15, '')
+TEST_RESULT_FONT = ('consolas', 30, 'bold')
 
 intro_message = 'Добро пожаловать в тестирующую систему!\n' \
                 'Вам предстоит пройти нелегкое испытание, в ходе которого Вас проверят на знание информационных технологий.\n' \
